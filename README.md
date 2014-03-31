5058f03a1f3c41fb56f9a508599e00c80862ad14
Kenny Rasschaert <kenny@inuits.eu>
2014-03-31 09:57:54 +0200

    Added a debouncing sketch that uses the Arduino example "Debounce" and puts the debouncing into a separate function. Clarified the licenses in README.md

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ beginner-arduino-workshop
 
 Sketches, diagrams and notes for the Arduino workshop for beginners, given on Arduino Day 2014 at the Inuits office.
 
-The presentation was made using [reveal.js](https://github.com/hakimel/reveal.js) and is available online in both [English](http://rasschaert.github.io/beginner-arduino-workshop/presentation/en.html) and [Dutch](http://rasschaert.github.io/beginner-arduino-workshop/presentation/nl.html).
+The presentation is available online in both [English](http://rasschaert.github.io/beginner-arduino-workshop/presentation/en.html) and [Dutch](http://rasschaert.github.io/beginner-arduino-workshop/presentation/nl.html).
 .
 
 ###License
-[Creative Commons Attribution 4.0 International](http://creativecommons.org/licenses/by/4.0/).
+The contents of the presentation are licensed under [Creative Commons Attribution 4.0 International](http://creativecommons.org/licenses/by/4.0/). The presentation was made using [reveal.js](https://github.com/hakimel/reveal.js) which was written by Hakim El Hattab and is available under the MIT license.
+
+All example code in the 'code' folder is in the public domain.
---
+++ code/debouncylib.ino
@@ -0,0 +1,73 @@
+/*
+  This code uses the Debounce.ino example and puts the debouncing logic into a separate function.
+  It expects the button to be wired to a pulldown resistor.
+  The hard parts were written by David A. Mellis, Limor Fried and Mike Walters,
+  who kindly released it into the public domain. The same license applies to this code.
+*/
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 2;    // the number of the pushbutton pin
+
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+int counter = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  boolean pressed = debounce();
+  if (pressed == true) {
+    counter++;
+    Serial.print("Button pressed ");
+    Serial.print(counter);
+    Serial.println(" times");
+  }
+}
+
+void debounce() {
+  boolean retVal = false;
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        retVal = true
+      }
+    }
+  }
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+
+  return retVal;
+}
+
